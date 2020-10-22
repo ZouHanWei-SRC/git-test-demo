e141c8283b53e18ff8f596e38f29092828d7647c
zouhw <hrvyzou.cju@outlook.com>
2020-10-22 13:24:37 +0800

    第一次提交

---
+++ src/test/java/com/hrvy/demo/GitTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrvy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
